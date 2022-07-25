1f2ae0df1c4b1354072419138f14124110bc43a0
MohammadMahdi Jarrahi <jarrahi.mm@gmail.com>
2022-07-25 12:50:27 +0430

    add BooleanQuranIR and remove boolean_retrieval.py

--- quran_ir.py
+++ quran_ir.py
@@ -1,9 +1,5 @@
-import shlex
 from abc import abstractmethod
-
-from sklearn.feature_extraction.text import TfidfVectorizer
 from preprocess_quran_text import merged_quran_vec_df_nrmlz, quran_normalizer
-from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
 import numpy as np
 
@@ -35,9 +31,52 @@ class QuranIR:
         return results
 
 
+class BooleanQuranIR(QuranIR):
+
+    def __init__(self):
+        from preprocess_quran_text import verse_complete_dict_nrmlz, verse_lemma_dict_nrmlz, verse_root_dict_nrmlz, \
+            verse_complete_dict
+        from boolean_retrieval.ir_system import IRSystem
+        super().__init__()
+        self.algorithm = 'Boolean'
+        # TODO: use merged_quran_df
+        self.keys = [*verse_complete_dict.keys()]
+        self.docs, self.docs_complete, self.docs_lemma, self.docs_root = [*verse_complete_dict.values()], [
+            *verse_complete_dict_nrmlz.values()], [*verse_lemma_dict_nrmlz.values()], [*verse_root_dict_nrmlz.values()]
+        self.boolean_ir_complete, self.boolean_ir_lemma, self.boolean_ir_root = IRSystem(self.docs_complete), IRSystem(
+            self.docs_lemma), IRSystem(self.docs_root)
+
+    def get_most_similars(self, original_corpus: pd.Series, query: str, K=10, check_moghattaeh=False) -> pd.DataFrame:
+        result = self.boolean_ir_complete.process_query(query, "complete")
+        results_lemma = self.boolean_ir_lemma.process_query(query, "lemma")
+        results_root = self.boolean_ir_root.process_query(query, "root")
+        result.extend([r for r in results_lemma if r not in result])
+        result.extend([r for r in results_root if r not in result])
+        result = pd.DataFrame(data={'آیه': [self.docs[r] for r in result]}, index=[self.keys[r] for r in result])
+        return result.iloc[:K]
+
+    def process_queries(self, query_path='./queries.txt', result_path='ir_responses/') -> pd.DataFrame:
+        with open('./queries_boolean.txt') as f:
+            queries = f.readlines()
+            queries = [q.strip().split() for q in queries]
+
+        results = []
+        i = 1
+        for query in queries:
+            # results.append('q{} = "{}"'.format(i, ' '.join(query)))
+            results.append({'Query': 'q{} = "{}"'.format(i, ' '.join(query))})
+            results.extend(self.get_most_similars(None, query).to_dict('records'))
+            i += 1
+
+        results = pd.DataFrame(results)
+        results.to_csv(f'{result_path}{self.algorithm}.csv', index=False)
+        return results
+
+
 class TfIdfQuranIR(QuranIR):
 
     def __init__(self):
+        from sklearn.feature_extraction.text import TfidfVectorizer
         super().__init__()
         self.algorithm = 'TfIdf'
         corpus = merged_quran_vec_df_nrmlz.fillna(' ').to_numpy().flatten('F').tolist()
@@ -49,6 +88,7 @@ class TfIdfQuranIR(QuranIR):
         self.median_idf = (np.max(self.idf_mat) + np.min(self.idf_mat)) / 2
 
     def get_most_similars(self, original_corpus: pd.Series, query: str, K=10, check_moghattaeh=False) -> pd.DataFrame:
+        from sklearn.metrics.pairwise import linear_kernel
         embedded_query = self.vectorizer.transform([query])
         cosine_similarities = linear_kernel(self.doc_term_matrix, embedded_query).flatten()
 
@@ -79,6 +119,7 @@ class FasttextQuranIR(QuranIR):
     def __init__(self):
         import fasttext
         super().__init__()
+        self.algorithm = 'Fasttext'
         self.model = fasttext.load_model('fasttext_model/model.bin')
         self.tfidf_quran_ir = TfIdfQuranIR()
         self.merged_corpus_embeddings = merged_quran_vec_df_nrmlz.applymap(self.sent_to_vec)
@@ -87,6 +128,7 @@ class FasttextQuranIR(QuranIR):
     def train(create_dataset=False):
         from tools import create_data_set
         import subprocess
+        import shlex
         if create_dataset:
             create_data_set(out_dir='./fasttext_model/data_set.txt', merged_df=merged_quran_vec_df_nrmlz,
                             expansion_count=5, lemma_rate=0.1, root_rate=0.4)
@@ -109,7 +151,7 @@ class FasttextQuranIR(QuranIR):
     def get_most_similars(self, original_corpus: pd.Series, query: str, K=10, check_moghattaeh=False) -> pd.DataFrame:
         import tools
         return tools.get_most_similars(original_corpus=original_corpus,
-                                 merged_corpus_embeddings=self.merged_corpus_embeddings,
-                                 query_vec=self.sent_to_vec(query),
-                                 K=10,
-                                 check_moghattaeh=check_moghattaeh)
+                                       merged_corpus_embeddings=self.merged_corpus_embeddings,
+                                       query_vec=self.sent_to_vec(query),
+                                       K=10,
+                                       check_moghattaeh=check_moghattaeh)
